39d330173f6247fbf07b2956bd87706cacf83666
Gathell <vladislav.k0707@gmail.com>
2021-02-11 16:13:39 +0300

    Программы из книги, по теме ввод-вывод данны

---
+++ user_input.py
@@ -0,0 +1,11 @@
+def reverse(text):
+    return text[::-1]
+
+def is_palindrome(text):
+    return text == reverse(text)
+
+sometheing = input('Введите текст: ')
+if (is_palindrome(sometheing)):
+    print('Да, это палиндром')
+else:
+    print("Нет, это не палиндром")
